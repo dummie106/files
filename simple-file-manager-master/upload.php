610963eef8a6b574d2f2f23120bdb9a0db7f8065
dummie106 <71605111+dummie106@users.noreply.github.com>
2021-01-26 22:12:41 -0500

    Update upload.php

--- simple-file-manager-master/upload.php
+++ simple-file-manager-master/upload.php
@@ -1,1 +1,14 @@
 
+<?php
+$folder = "uploads";
+if (is_uploaded_file($_FILES['filename']['tmp_name']))  {  
+    if (move_uploaded_file($_FILES['filename']['tmp_name'],
+ $folder."/".$_FILES['filename']['name'])) {
+         Echo "File uploaded";
+    } else {
+         Echo "File not moved to destination folder. Check permissions";
+    };
+} else {
+     Echo "File is not uploaded.";
+};
+?>
